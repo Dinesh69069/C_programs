fa5f89969ea9dcdc212c0c21f999e1a8eb19d853
DINESH KUMAR SAHOO <dineshkumarcs0014@gmail.com>
2024-11-02 15:27:18 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+int main()
+{
+        int n,r,s=0;
+        printf("Enter a number:");
+        scanf("%d",&n);
+
+        while(n>0)
+                {
+                        r=n%10;
+                        s=s*10+r;
+                        n=n/10;
+
+                }
+        printf("The reversed number is :%d",s);
+
+}
+                         
